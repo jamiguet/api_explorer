acaf742398a79489da7724c487bf11bcd418da8e
Juan Amiguet <jamiguet@gmail.com>
2024-06-04 00:16:52 +0200

    Update Metrics Explorer and add new documentation
    Added two new documentation slides for better understanding of the project. Performed major refactor in Metrics Explorer for optimized initialization of sessions and exchanges. This includes handling of assets and resolution, extraction of active exchanges and metric tiers.

--- pages/Metric_Market_Explorer.py
+++ pages/Metric_Market_Explorer.py
@@ -1,15 +1,23 @@
-import streamlit as st
-from util import init_session, get_all_metrics
 import json
+
+import streamlit as st
+
+from util import init_session, get_all_metrics
 
 init_session()
 st.title("Metric Market Explorer")
 
-active_exchanges = st.sidebar.multiselect("Select Exchanges", options=['kucoin', 'okx', 'binance'])
-tiers = set(map(lambda entry: entry['tier'], get_all_metrics()))
-current_tier =st.sidebar.selectbox("Select metric tier", tiers)
+available_exchanges = {'kucoin': st.session_state.ku_markets,
+                       'okx': st.session_state.okx_markets,
+                       'binance': st.session_state.binance_markets}
 
-filtered_metrics = list(filter(lambda item: item['tier'] in tiers ,get_all_metrics()))
+active_exchanges = st.sidebar.multiselect("Select Exchanges", options=available_exchanges.keys(), default='kucoin')
+
+
+tiers = set(map(lambda entry: entry['tier'], get_all_metrics()))
+current_tier = st.sidebar.selectbox("Select metric tier", tiers)
+
+filtered_metrics = list(filter(lambda item: item['tier'] in tiers, get_all_metrics()))
 
 metric_names = list(map(lambda entry: entry['name'], filtered_metrics))
 metrics = st.sidebar.multiselect("Select Metrics", options=metric_names)
@@ -25,10 +33,13 @@ st.json(json.dumps(list(map(lambda entry: entry['resolutions'], filtered_metrics
 resolutions = set(list(map(lambda entry: entry['resolutions'], filtered_metrics))[0])
 assets = list()
 for entry in map(lambda entry: entry['assets'], filtered_metrics):
-    assets.extend(list(map(lambda item: item['symbol'], entry)))
+    current_assets = list(map(lambda item: item['symbol'], entry))
+    current_assets = list(filter(lambda item: item in assets, current_assets))
+    assets.extend(current_assets)
 
-asset = st.sidebar.selectbox("Select coin",options=assets)
-resolution = st.sidebar.selectbox("Select resolution",options=resolutions)
+assets = set(assets)
+asset = st.sidebar.selectbox("Select coin", options=assets)
+resolution = st.sidebar.selectbox("Select resolution", options=resolutions)
 
 for current_metric in metrics:
-    st.subheader(current_metric.replace('_','').title())
+    st.subheader(current_metric.replace('_', ' ').title())
--- pages/Metrics_Explorer.py
+++ pages/Metrics_Explorer.py
@@ -3,54 +3,26 @@ import datetime
 import pandas as pd
 import streamlit as st
 from util import call, plot_transfer_count, load_pump_data, get_field_set, get_field_filtered_list, to_unix, \
-    plot_candles, plot_series, annotate_symbol, get_all_metrics
+    plot_candles, plot_series, annotate_symbol, get_all_metrics, init_session
 from io import StringIO
 import ccxt
 from ccxt.kucoin import BadSymbol
 from functools import reduce
 import plotly.express as px
 
-db = st.connection("postgresql", type="sql")
+init_session()
 
-kucoin = ccxt.kucoin({
-    'adjustForTimeDifference': True,
-    "apiKey": st.secrets.KUCOIN_API_KEY,
-    "secret": st.secrets.KUCOIN_API_SECRET,
-    'password': st.secrets.PASSWORD,
-})
+available_exchanges = {'kucoin': st.session_state.ku_markets,
+                       'okx': st.session_state.okx_markets,
+                       'binance': st.session_state.binance_markets}
 
-binance = ccxt.binance({
-    'apiKey': st.secrets.BINANCE_API_KEY,
-    'secret': st.secrets.BINANCE_API_SECRET,
-    'enableRateLimit': True,
-    'options': {
-        'defaultType': 'spot',
-    },
-})
+active_exchanges = st.sidebar.multiselect("Select Exchanges", options=available_exchanges.keys(), default='kucoin')
 
-okx = ccxt.okx({
-    'apiKey': st.secrets.OKX_API_KEY,
-    'secret': st.secrets.OKX_API_SECRET,
-    'enableRateLimit': True
-})
 
-kucoin.load_markets()
-binance.load_markets()
-okx.load_markets()
+tiers = set(map(lambda entry: entry['tier'], get_all_metrics()))
+current_tier = st.sidebar.selectbox("Select metric tier", tiers)
 
-ku_markets = list(
-    set(map(lambda symbol: symbol.split('/')[0],
-            filter(lambda symbol: 'USDT' in symbol, kucoin.symbols))))
-
-binance_markets = list(
-    set(map(lambda symbol: symbol.split('/')[0],
-            filter(lambda symbol: 'USDT' in symbol, binance.symbols))))
-
-okx_markets = list(
-    set(map(lambda symbol: symbol.split('/')[0],
-            filter(lambda symbol: 'USDT' in symbol, okx.symbols))))
-
-pumped_coins = load_pump_data(db)
+pumped_coins = load_pump_data(st.session_state.db)
 
 assets = call("https://api.glassnode.com/v1/metrics/assets")
 field_set = list(set([item for sublist in get_field_set('tags', assets) for item in sublist]))
@@ -63,9 +35,9 @@ metrics_list.sort(key=lambda item: item['path'])
 supported_coins = [asset['symbol'] for asset in reduce(lambda acc, val: acc + val['assets'], metrics_list, [])]
 
 filtered_assets = get_field_filtered_list('symbol', tag, assets)
-filtered_assets = list(filter(lambda symbol: (symbol in ku_markets or symbol in binance_markets) and symbol in supported_coins , filtered_assets))
+filtered_assets = list(filter(lambda symbol: (symbol in st.session_state.ku_markets or symbol in st.session_state.binance_markets) and symbol in supported_coins , filtered_assets))
 filtered_assets = list(
-    map(lambda symbol: annotate_symbol(symbol, pumped_coins, ku_markets, binance_markets, okx_markets), filtered_assets))
+    map(lambda symbol: annotate_symbol(symbol, pumped_coins, st.session_state.ku_markets, st.session_state.binance_markets, st.session_state.okx_markets), filtered_assets))
 current_asset = st.sidebar.selectbox(f"Assets ({len(filtered_assets)})", filtered_assets).split(' ')[0]
 
 start = st.sidebar.date_input('Start Date', datetime.date.today() - datetime.timedelta(days=60))
@@ -82,10 +54,14 @@ st.title('Metrics Explorer')
 current_metric = st.sidebar.multiselect('Select metric:', metrics_list,
                                         format_func=lambda item: item['path'].split('/')[-1])
 
+if len(current_metric) == 0:
+    st.info('Please select a metric')
+    st.stop()
+
 for metric in current_metric:
     path = metric['path']
     metric_name = path.split('/')[-1]
-    st.text(f'{metric_name}')
+    st.text(f'{metric_name.replace("_", " ").title()}')
 
     params = {
         'a': current_asset,
@@ -109,29 +85,16 @@ for metric in current_metric:
 
     num_candles = int((end - start).total_seconds() / 3600 / (24 if resolution == '24h' else 1))
 
-    try:
-        kucoin_candles = kucoin.fetch_ohlcv(f"{current_asset}-USDT", timeframe=kucoin_res[resolution],
-                                            since=to_unix(start) * 1000,
-                                            limit=num_candles)
+    for exchange in active_exchanges:
+        if exchange == 'kucoin':
+            symbol_separator = '/'
+        else:
+            symbol_separator = '-'
+        try:
+            candles = st.session_state.kucoin.fetch_ohlcv(f"{current_asset}{symbol_separator}USDT", timeframe=kucoin_res[resolution],
+                                                since=to_unix(start) * 1000,
+                                                limit=num_candles)
 
-        plot_candles(kucoin_candles, f'{current_asset} Kucoin')
-    except BadSymbol as bs:
-        st.error(f"Bad symbol: {bs}")
-
-    try:
-        binance_candles = binance.fetch_ohlcv(f"{current_asset}/USDT", timeframe=kucoin_res[resolution],
-                                              since=to_unix(start) * 1000,
-                                              limit=num_candles)
-
-        plot_candles(binance_candles, f'{current_asset} Binanace')
-    except BadSymbol as bs:
-        st.error(f"Bad symbol: {bs}")
-
-    try:
-        okx_candles = okx.fetch_ohlcv(f"{current_asset}/USDT", timeframe=kucoin_res[resolution],
-                                              since=to_unix(start) * 1000,
-                                              limit=num_candles)
-
-        plot_candles(okx_candles, f'{current_asset} OKX')
-    except BadSymbol as bs:
-        st.error(f"Bad symbol: {bs}")
+            plot_candles(candles, f'{current_asset} {exchange.title()}')
+        except BadSymbol as bs:
+            st.error(f"Bad symbol: {bs}")
